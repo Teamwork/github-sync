8249fad9c60396f02ebbac44f0276c5c67e6fc06
Rafael Dantas Justo <rafael.justo@teamwork.com>
2020-09-17 14:40:04 +0100

    Fix: Warnings detected by the linter

--- src/teamwork.sh
+++ src/teamwork.sh
@@ -5,12 +5,13 @@ teamwork::get_task_id_from_body() {
   local task_ids=()
 
   pat='tasks\/([0-9]{1,})'
-  while [[ $body =~ $regex ]]; do
-    task_ids+=( ${BASH_REMATCH[1]} )
+  while [[ $body =~ $pat ]]; do
+    task_ids+=( "${BASH_REMATCH[1]}" )
     body=${body#*"${BASH_REMATCH[1]}"}
   done
 
-  local task_ids_str=$(printf ",%s" "${task_ids[@]}")
+  local task_ids_str
+  task_ids_str=$(printf ",%s" "${task_ids[@]}")
   task_ids_str=${task_ids_str:1} # remove initial comma
   echo "$task_ids_str"
 }
