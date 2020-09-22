0837030ed8cc36489a6550223752acb58b9a577a
Rafael Dantas Justo <rafael.justo@teamwork.com>
2020-09-22 12:55:32 +0100

    Fix: Notify users about new task comment
    Resolves #23

--- src/teamwork.sh
+++ src/teamwork.sh
@@ -27,7 +27,7 @@ teamwork::add_comment() {
   response=$(curl -X "POST" "$TEAMWORK_URI/projects/api/v1/tasks/$TEAMWORK_TASK_ID/comments.json" \
        -u "$TEAMWORK_API_TOKEN"':' \
        -H 'Content-Type: application/json; charset=utf-8' \
-       -d "{ \"comment\": { \"body\": \"${body//\"/}\", \"notify\": \"\", \"content-type\": \"text\", \"isprivate\": false } }" )
+       -d "{ \"comment\": { \"body\": \"${body//\"/}\", \"notify\": true, \"content-type\": \"text\", \"isprivate\": false } }" )
 
   log::message "$response"
 }
